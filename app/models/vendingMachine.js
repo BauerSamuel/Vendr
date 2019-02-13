0032019a9f1466b614579fdf24871c9be6228fee
Samuel Bauer <bauersam@outlook.com>
2019-02-12 21:49:40 -0700

    margins and images

--- app/models/vendingMachine.js
+++ app/models/vendingMachine.js
@@ -4,9 +4,9 @@ export default class VendingMaching {
   //code here.
   constructor() {
     this.snacks = {
-      a1: new Snack('chips', 1.50, '../../assets/images/Chips.png'),
-      a2: new Snack('candy', 2.00, '../../assets/images/Candy.png'),
-      a3: new Snack('choc', 1.75, '../../assets/images/Choc.png'),
+      a1: new Snack('chips', 1.50, 'assets/images/Chips.png'),
+      a2: new Snack('candy', 2.00, 'assets/images/Candy.png'),
+      a3: new Snack('choc', 1.75, 'assets/images/Choc.png'),
     }
     this.balance = 0;
   }
